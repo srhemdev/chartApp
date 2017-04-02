0a5be6ffb4c450c1468397e04ea5ce12f3f3bfae
shwetahemdev <shweta.hemdev@elementum.com>
2017-04-01 20:29:49 -0700

    adding error message

--- app/chart-view/directives/form-directive/form-directive.js
+++ app/chart-view/directives/form-directive/form-directive.js
@@ -9,9 +9,21 @@ angular.module('myApp.chartView')
             },
             templateUrl: 'chart-view/directives/form-directive/form.html',
             link: function($scope, $element, $attrs){
-
+                $scope.hasError = false;
                 $scope.addData = function() {
-                    $scope.callback({data: $scope.form})
+                    $scope.hasError = !checkProperties($scope.form);
+                    if(!$scope.hasError) {
+                        $scope.callback({data: $scope.form})
+                    }
+                }
+
+                function checkProperties(obj) {
+                    if(!obj) return false;
+                    for (var key in obj) {
+                        if (obj[key] === null || obj[key] === "")
+                            return false;
+                    }
+                    return true;
                 }
 
             }
